48fc8b40ecbdb2df733426a482382fd912bb18c9
MoonLoveless <lexyls523@gmail.com>
2025-11-06 02:02:20 -0500

    feat(HU4): Implementa Toolbar, fondo y navegación para AddItemFragment (Criterio 1)

--- app/src/main/java/com/univalle/inventoryapp/view/fragments/AddItemFragment.kt
+++ app/src/main/java/com/univalle/inventoryapp/view/fragments/AddItemFragment.kt
@@ -1,28 +1,39 @@
 package com.univalle.inventoryapp.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.univalle.inventoryapp.R
 import com.univalle.inventoryapp.databinding.FragmentAddItemBinding
 
 class AddItemFragment : Fragment() {
+
+    // Variable para Data Binding
     private lateinit var binding: FragmentAddItemBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentAddItemBinding.inflate(inflater)
-        binding.lifecycleOwner = this
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_add_item, container, false
+        )
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-    }
 
+        binding.toolbarAddItem.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+
+    }
 }

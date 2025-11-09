18cc82d264283e804b688bf2952951e5dc7d346d
gamberfox <gamberfox@gmail.com>
2025-11-09 12:35:17 -0500

    HU1 avance: abrir la app

--- app/src/main/java/com/univalle/inventoryapp/view/MainActivity.kt
+++ app/src/main/java/com/univalle/inventoryapp/view/MainActivity.kt
@@ -4,9 +4,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.univalle.inventoryapp.R
 
+import android.content.Context
+const val PREFS_NAME1 = "com.univalle.inventoryapp.widget.WidgetPrefs"
+const val PREF_KEY_WIDGET_VALUE1 = "app_value"
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ////////////////
+        saveValueForWidget(this,"900000")
     }
+    //////////////////////////////////
+    //////////////////////////////////
+    fun saveValueForWidget(context: Context, value: String) {
+        val prefs = context.getSharedPreferences(PREFS_NAME1, Context.MODE_PRIVATE)
+        prefs.edit().putString(PREF_KEY_WIDGET_VALUE1, value).apply()
+    }
+    //////////////////////////////////
+    //////////////////////////////////
 }
--- app/src/main/java/com/univalle/inventoryapp/view/widget/WidgetProvider.kt
+++ app/src/main/java/com/univalle/inventoryapp/view/widget/WidgetProvider.kt
@@ -11,24 +11,32 @@ import com.univalle.inventoryapp.R
 import com.univalle.inventoryapp.view.MainActivity
 
 import android.graphics.Color
-
+import com.univalle.inventoryapp.view.PREFS_NAME1
+import com.univalle.inventoryapp.view.PREF_KEY_WIDGET_VALUE1
 
 
 class WidgetProvider: AppWidgetProvider() {
 
-    private var inventoryValue="showing"
+    private var inventoryValue="no data loaded"
 
     companion object {
         const val ACTION_TOGGLE_TEXT = "com.univalle.inventoryapp.widget.TOGGLE_TEXT"
         const val EXTRA_WIDGET_ID = "com.univalle.inventoryapp.extra.WIDGET_ID"
-        const val PREFS_NAME = "WidgetPrefs"
+        const val PREFS_NAME ="com.univalle.inventoryapp.widget.WidgetPrefs"
         const val PREF_PREFIX_KEY = "visible_value"
+        const val PREF_VALUE_KEY = "inventory_value"
         const val ACTION_CHANGE_TEXT = "com.example.mywidget.CHANGE_TEXT"
         const val ACTION_LOGIN = "com.univalle.inventoryapp.widget.LOGIN"
     }
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         for (appWidgetId in appWidgetIds) {
+            /////////////from the actvity////////////////////////
+            val prefs = context.getSharedPreferences(PREFS_NAME1, Context.MODE_PRIVATE)
+            val appValue = prefs.getString(PREF_KEY_WIDGET_VALUE1, "No data yet") as String
+            saveValue(context,appWidgetId,appValue)
+            ////////////////////////////////////
+            ////////////////////////////////////
             val views = RemoteViews(context.packageName, R.layout.widget)
             // Create intent to trigger a broadcast when the widget is clicked
             val intent = Intent(context, WidgetProvider::class.java).apply {
@@ -89,33 +97,26 @@ class WidgetProvider: AppWidgetProvider() {
 
         val views = RemoteViews(context.packageName, R.layout.widget)
         if (intent.action == ACTION_CHANGE_TEXT) {
-//            val appWidgetManager = AppWidgetManager.getInstance(context)
-//            val appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1)
-//
-//            val views = RemoteViews(context.packageName, R.layout.widget)
-
             // Random text or toggle between two values
-            val newText = listOf("inventario", "inventary", "Nice!").random()
+            val newText = listOf("inventario", "inventory", "Nice!").random()
             views.setTextViewText(R.id.widget_title, newText)
 
-//            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
         else if (intent.action == ACTION_TOGGLE_TEXT) {
-//            val appWidgetId = intent.getIntExtra(EXTRA_WIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
-
             val current = loadVisibility(context, appWidgetId)
             val newState = !current
             saveVisibility(context, appWidgetId, newState)
 
+//            val inventoryValue0=loadValue(context,appWidgetId)
+            val inventoryValue0=loadValueLive(context,appWidgetId)
             if(current){
                 views.setTextViewText(R.id.widget_value, "$****")
                 views.setImageViewResource(R.id.eye_icon, R.drawable.ic_closed_eye)
             }
             else{
-                views.setTextViewText(R.id.widget_value, inventoryValue)
+                views.setTextViewText(R.id.widget_value, inventoryValue0)
                 views.setImageViewResource(R.id.eye_icon, R.drawable.ic_opened_eye)
             }
-//            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
         else if (intent.action == ACTION_LOGIN) {
             val launchIntent = Intent(context, MainActivity::class.java)
@@ -125,7 +126,7 @@ class WidgetProvider: AppWidgetProvider() {
 
         appWidgetManager.updateAppWidget(appWidgetId, views)
     }
-    private fun saveVisibility(context: Context, appWidgetId: Int, visible: Boolean) {
+    fun saveVisibility(context: Context, appWidgetId: Int, visible: Boolean) {
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         prefs.edit().putBoolean(PREF_PREFIX_KEY + appWidgetId, visible).apply()
     }
@@ -134,5 +135,19 @@ class WidgetProvider: AppWidgetProvider() {
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         return prefs.getBoolean(PREF_PREFIX_KEY + appWidgetId, false)
     }
-
+    fun saveValue(context: Context, appWidgetId: Int, value: String) {
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        prefs.edit().putString(PREF_VALUE_KEY + appWidgetId, value).apply()
+    }
+    private fun loadValue(context: Context, appWidgetId: Int): String? {
+//        val prefs = context.getSharedPreferences(PREFS_NAME1, Context.MODE_PRIVATE)
+//        val appValue = prefs.getString(PREF_KEY_WIDGET_VALUE1, "No data yet") as String
+        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        return prefs.getString(PREF_VALUE_KEY + appWidgetId, "-65.00")
+    }
+    private fun loadValueLive(context: Context, appWidgetId: Int): String? {
+        val prefs = context.getSharedPreferences(PREFS_NAME1, Context.MODE_PRIVATE)
+        val appValue = prefs.getString(PREF_KEY_WIDGET_VALUE1, "No data yet") as String
+        return appValue
+    }
 }

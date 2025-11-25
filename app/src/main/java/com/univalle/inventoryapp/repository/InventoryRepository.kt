ac03818f6be2c707cbf05460fef6a8c1783272eb
Christian Vargas <vargas.christian@correounivalle.edu.co>
2025-11-24 23:19:58 -0500

    Corrección inyección de dependencias a nivel de InventoryRepository

--- app/src/main/java/com/univalle/inventoryapp/repository/InventoryRepository.kt
+++ app/src/main/java/com/univalle/inventoryapp/repository/InventoryRepository.kt
@@ -1,9 +1,6 @@
 package com.univalle.inventoryapp.repository
 
-import android.content.Context
 import com.google.firebase.firestore.FirebaseFirestore
-import com.univalle.inventoryapp.data.InventoryDB
-import com.univalle.inventoryapp.data.InventoryDao
 import com.univalle.inventoryapp.model.Inventory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -11,9 +8,8 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class InventoryRepository @Inject constructor(
-    private val inventoryDao: InventoryDao
+    private val db : FirebaseFirestore
 ){
-    private val db = FirebaseFirestore.getInstance()
 
     suspend fun getListInventory():MutableList<Inventory>{
         return withContext(Dispatchers.IO){
@@ -45,8 +41,8 @@ class InventoryRepository @Inject constructor(
 
 
     suspend fun insertProduct(inventory: Inventory){
-        withContext(Dispatchers.IO){
-            inventoryDao.insertProduct(inventory)
+        return withContext(Dispatchers.IO){
+//            inventoryDao.insertProduct(inventory)
         }
     }
 
